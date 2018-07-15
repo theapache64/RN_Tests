260c967aa1b315cf064200a7aed1a4659c3d5ebb
theapache64 <theapache64@gmail.com>
2018-07-15 09:44:55 +0530

    Verified

--- App.tsx
+++ App.tsx
@@ -8,25 +8,39 @@
  * @format
  */
 
-import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import React, { Component } from 'react';
+import { Platform, StyleSheet, Text, View } from 'react-native';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+interface BaseScreenState {
+  baseClassContent: string;
+}
 
-interface Props {}
-export default class App extends Component<Props> {
+abstract class BaseScreen<P={}, S={}> extends Component<P, S & BaseScreenState>{
+
+  abstract renderScreen(): any;
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native :)</Text>
-        <Text style={styles.instructions}>To get started, edit App.tsx</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
+      <View>
+        <Text>{this.state.baseClassContent}</Text>
+        {this.renderScreen()}
       </View>
+    );
+  }
+}
+
+interface State {
+  appContent: string;
+}
+export default class App extends BaseScreen<{}, State> {
+
+  state = {
+    baseClassContent: 'Hello',
+    appContent: 'This is App class content'
+  }
+
+  renderScreen() {
+    return (
+      <Text>{this.state.appContent}</Text>
     );
   }
 }
@@ -37,15 +51,5 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+  }
 });
